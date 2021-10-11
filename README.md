f175cea437eb0e2aed2f1b79a5a6fca6c9220100
yuimatcha <faridsaja12@gmail.com>
2021-10-11 14:56:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 👷 Security Headers
+
+A Cloudflare worker project for security headers.
+
+#### Wrangler
+
+```bash
+wrangler dev
+```
+
+Further documentation for Wrangler can be found [here](https://developers.cloudflare.com/workers/cli-wrangler).
---
+++ index.js
@@ -0,0 +1,29 @@
+let addHeaders = {
+  "Content-Security-Policy": "upgrade-insecure-requests",
+  "Strict-Transport-Security": "max-age=2592000",
+  "X-XSS-Protection": "1; mode=block",
+  "X-Frame-Options": "SAMEORIGIN",
+  "X-Content-Type-Options": "nosniff",
+  "Referrer-Policy": "same-origin"
+}
+
+// 
+addEventListener("fetch", event => {
+  event.respondWith(fetchAndApply(event.request))
+})
+
+async function fetchAndApply(request) {
+  // Fetch the original page from the origin
+  let response = await fetch(request)
+  
+  // Make response headers mutable
+  response = new Response(response.body, response)
+
+  // Set each header in addHeaders
+  Object.keys(addHeaders).map(function(name, index) {
+    response.headers.set(name, addHeaders[name])
+  })
+
+  // Return the new mutated page
+  return response
+}
